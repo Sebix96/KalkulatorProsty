816b10b5f003f9e3bf7a59abb4c627dfd7172bc8
Sebix96 <44363688+Sebix96@users.noreply.github.com>
2019-01-25 22:13:55 +0100

    Refactor_fix_final

--- app/src/main/java/com/example/sebastian/kalkulatorprosty/MainActivity.java
+++ app/src/main/java/com/example/sebastian/kalkulatorprosty/MainActivity.java
@@ -6,17 +6,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
-
 import net.objecthunter.exp4j.Expression;
 import net.objecthunter.exp4j.ExpressionBuilder;
 
-
 public class MainActivity extends AppCompatActivity {
 
-    StringBuilder textEquation = new StringBuilder();
-
-
-
+    private StringBuilder textEquation = new StringBuilder();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,75 +20,69 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
-    public void refreshText()
-    {
+
+    private void refreshText() {
         TextView textView = findViewById(R.id.ViewM);
         textView.setText(textEquation.toString());
     }
 
-    public void addToEquation(View view)
-    {
-        int lengthOfEguqtion = textEquation.length()-1;
-        TextView button = (TextView)view;
+    public void addToEquation(View view) {
+        int lengthOfEguqtion = textEquation.length() - 1;
+        TextView button = (TextView) view;
         String character = button.getText().toString();
-        if(lengthOfEguqtion>14)
-        {
+        if (lengthOfEguqtion > 13) {
             return;
         }
         textEquation.append(character);
         refreshText();
     }
-    public void calculateEquation(View view)
-    {
+
+    public void calculateEquation(View view) {
         int lengthEquation = textEquation.length();
-        if(lengthEquation<1){
+        if (lengthEquation < 1) {
             return;
         }
         double result;
         try {
-        Expression expression = new ExpressionBuilder(textEquation.toString()).build();
-        result = expression.evaluate();
+            Expression expression = new ExpressionBuilder(textEquation.toString()).build();
+            result = expression.evaluate();
         } catch (Exception e) {
             result = Double.NaN;
         }
 
         TextView textView = findViewById(R.id.ViewM);
         textView.setText(String.valueOf(result));
-        if(Double.isNaN(result)){
-        }
-        else{
+        if (Double.isNaN(result)) {
+        } else {
             DatabaseHelper database = new DatabaseHelper(this);
-            database.setData(textEquation.toString()+" = "+result);
+            database.setData(textEquation.toString() + " = " + result);
         }
         textEquation.delete(0, textEquation.length());
 
 
     }
-    public void clearOne(View view)
-    {
+
+    public void clearOne(View view) {
         int lengthEquation = textEquation.length();
-        if(lengthEquation<1) {
+        if (lengthEquation < 1) {
             refreshText();
-        }
-        else
-        {
+        } else {
             textEquation.delete(textEquation.length() - 1, textEquation.length());
             refreshText();
         }
     }
-    public void clearAll(View view)
-    {
+
+    public void clearAll(View view) {
         int lengthEquation = textEquation.length();
-        if(lengthEquation<1) {
+        if (lengthEquation < 1) {
             refreshText();
-        }
-        else {
+        } else {
             textEquation.delete(0, textEquation.length());
             refreshText();
         }
     }
-    public void goHistory(View view)
-    {
+
+    public void goHistory(View view) {
         Intent intent = new Intent(MainActivity.this, StoryActivity.class);
         startActivity(intent);
     }
